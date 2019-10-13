3d8e794f5ea6c231d8eb5beb05c7136b6f386526
Damian Fronia <01112935@pw.edu.pl>
2019-10-13 13:38:56 +0200

    Basic models

--- DDAPandDAPsolver/DDAPandDAPsolver/Program.cs
+++ DDAPandDAPsolver/DDAPandDAPsolver/Program.cs
@@ -1,3 +1,4 @@
+using DDAPandDAPsolver.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace DDAPandDAPsolver
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
+            FileReader fileReader = new FileReader(new NetworkModel());
+            fileReader.ReadFile("net12_1");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
