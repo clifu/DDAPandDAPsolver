cef00670c28d3f06a3a9c39c9c551bd8d69e3e14
Damian Fronia <01112935@pw.edu.pl>
2019-10-15 20:52:03 +0200

    Basic solution model and PModel => P(demand,path) [if I am correct]

---
+++ DDAPandDAPsolver/DDAPandDAPsolver/Models/PModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDAPandDAPsolver.Models
+{
+
+    //P(demand,path)
+    public class PModel
+    {
+        private int demandId;
+        private int pathId;
+
+        public int DemandId
+        {
+            get => demandId;
+            set => demandId = value;
+        }
+
+        public int PathId
+        {
+            get => pathId;
+            set => pathId = value;
+        }
+
+        public PModel(int demandId, int pathId)
+        {
+            this.demandId = demandId;
+            this.pathId = pathId;
+        }
+    }
+}
---
+++ DDAPandDAPsolver/DDAPandDAPsolver/Models/SolutionModel.cs
@@ -0,0 +1,45 @@
+﻿using DDAPandDAPsolver.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDAPandDAPsolver
+{
+    public class SolutionModel
+    {
+        private float networkCost;
+
+        public float NetworkCost
+        {
+            get => networkCost;
+            set => networkCost = value;
+        }
+
+        private List<int> linkCapacities = new List<int>();
+
+        public List<int> LinkCapacities
+        {
+            get => linkCapacities;
+            set => linkCapacities = value;
+        }
+
+        //x(d,p)=a PModel = (d,p), a = value
+        private Dictionary<PModel, int> xesList = new Dictionary<PModel, int>();
+
+        public Dictionary<PModel, int> XesList
+        {
+            get => xesList;
+            set => xesList = value;
+        }
+
+
+        public SolutionModel(Dictionary<PModel, int> xesList)
+        {
+            this.xesList = xesList;
+        }
+
+
+    }
+}
